98744602e513839f9dbd8256e123946b2677be09
Abhishek More <abhimore74@gmail.com>
2024-06-19 21:21:01 -0400

    added metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,44 @@
 import { ResourceLink } from "@/components/ResourceLink";
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Abhishek More",
+    description: "Experience Peak.",
+    url: "https://abhishekmore.com",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://abhishekmore.com/ogimage.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://abhishekmore.com/ogimage.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleLoad = () => {
     console.log("Loaded!");
-    setLoaded(true);
+    setLoading(false);
   };
 
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-black">
       {/* Overlay */}
-      <div className="absolute bottom-8 left-8 flex gap-2">
+      <div className="absolute bottom-8 left-8 flex gap-2 z-50">
         <ResourceLink
           href={
             "https://firebasestorage.googleapis.com/v0/b/asciify-a4fba.appspot.com/o/Abhishek%20More%20Resume.pdf?alt=media&token=612342c0-9664-41ce-beb2-b4265b20002b"
@@ -35,11 +60,17 @@ export default function Home() {
         <p className="text-white text-7xl font-bold">MORE</p>
       </div>
 
-      <Spline
-        scene="https://prod.spline.design/x9AvaSbmi1rQ3uaQ/scene.splinecode"
-        onLoad={handleLoad}
-        className={`absolute z-50`}
-      />
+      {loading && <p className="text-white">Loading</p>}
+
+      <div
+        className={`${loading ? "opacity-0" : "opacity-100"} transition-opacity duration-1000 delay-1000`}
+      >
+        <Spline
+          scene="https://prod.spline.design/x9AvaSbmi1rQ3uaQ/scene.splinecode"
+          onLoad={handleLoad}
+          className={`absolute z-40`}
+        />
+      </div>
     </div>
   );
 }
